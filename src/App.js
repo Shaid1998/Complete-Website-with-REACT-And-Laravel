57e7fd75481e32afd883a06914a95dbdcf386610
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-09 18:09:38 +0600

    Working on TopBanner Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TopBanner from './components/TopBanner/TopBanner.jsx';
 
 
 function App() {
   return (
     <div>
-      
+      <TopBanner />
     </div>
   );
 }
---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react'
+import { Container } from 'react-bootstrap'
+import '../../asset/css/custom.css'
+
+class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container className="topFixedBanner">
+
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default TopBanner
