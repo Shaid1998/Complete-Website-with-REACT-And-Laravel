ba3b7c45b264f9bd685a1c208a5080f19b055202
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-09 21:10:57 +0600

    Working on Top Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopBanner from './components/TopBanner/TopBanner.jsx';
 import Services from './components/Services/Services.jsx';
+import TopNavigation from './components/TopNavigation/TopNavigation.jsx';
 
 
 function App() {
   return (
     <div>
+      <TopNavigation />
       <TopBanner />
       <Services />
     </div>
