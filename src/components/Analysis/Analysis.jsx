f64aabe1bab18c63025a9cd54a401386fca35066
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-10 16:33:12 +0600

    Working On Bar Chart

--- src/components/Analysis/Analysis.jsx
+++ src/components/Analysis/Analysis.jsx
@@ -1,18 +1,65 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
 
 export class Analysis extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      data:[
+        {Technology:'PHP', Skilled:90},
+        {Technology:'Mysql', Skilled:90},
+        {Technology:'Laravel', Skilled:95},
+        {Technology:'ReactJs', Skilled:70},
+        {Technology:'VueJs', Skilled:50},
+        {Technology:'Java', Skilled:70},
+        {Technology:'JavaScript', Skilled:80},
+        {Technology:'Kotlin', Skilled:60}
+      ]
+    }
+  }
   render() {
+    var blue='#051b35';
     return (
       <Fragment>
-        <Container>
+        <Container className='text-center'>
+            <h1 className='ServiceMainTitle'>TECHNOLOGY</h1>
+            <div className='bottom'></div>
             <Row>
-                <Col>
-                    <h1>Bar Chart</h1>
+                <Col style={{width:'50%' , height:'350px'}} lg={6} md={12} sm={12}>
+                  <ResponsiveContainer>
+                      <BarChart width={100} height={350} data={this.state.data}>
+                        <XAxis dataKey={"Technology"}/>
+                        <Tooltip />
+                        <Bar dataKey="Skilled" fill={blue}>
+
+                        </Bar>
+
+                        
+
+                      </BarChart>
+                   </ResponsiveContainer>
                 </Col>
 
-                <Col>
-                    <h1>Text</h1>
+                <Col lg={6} md={12} sm={12}>
+                    <p className="text-justify serviceDescription"> As a recent graduate with a Bachelor of Science in Software
+                        Engineering from Daffodil International University, I possess a strong
+                        foundation in programming languages, including PHP, JavaScript, and
+                        SQL. Although I may be an entry-level candidate, I am confident that
+                        my skills and enthusiasm make me an excellent candidate for this
+                        position.<br></br><br></br>
+
+                        During my time at Daffodil International University University, I
+                        completed multiple projects that enhanced my understanding of web
+                        development and PHP. In one of my major projects.<br></br><br></br>
+
+                        Collaboration and adaptability are two essential qualities that I have
+                        refined through group projects  and I am eager to apply these skills
+                        in a professional setting. Furthermore, I am continuously seeking to
+                        expand my skill set and stay current with industry trends by
+                        participating in coding challenges .<br></br><br></br>
+                      </p>
                 </Col>
             </Row>
         </Container>
