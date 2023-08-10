fcdf45e292c53c921b5f81ad4bdd36cae2c50040
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-10 19:25:32 +0600

    Working On Summary Part

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,50 +1,55 @@
+
 import React, { Component, Fragment } from 'react'
-import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 
 class Summary extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Container fluid={true} className="SummaryFixedBanner p-0">
+     render() {
+          return ( 
+              <Fragment>   
+                <Container fluid={true} className="SummaryFixedBanner p-0" >
                     <div className='SummaryBannerOverLay'>
-                        <Container className='text-center'>
+                        <Container className="text-center">
                             <Row>
-                                <Col className='text-center' lg={8} md={6} sm={12}>
-                                    <Row className='countSelection'>
+                                <Col className="countSection" lg={8} md={6} sm={12}>
+                                    <Row>
                                         <Col>
                                             <h1 className='countNumber'>15+</h1>
                                             <h4 className='countTitle'>Projects Completed</h4>
-                                            <hr className="bg-white w-25"/>
+                                            <hr className='bg-white w-25'/>
                                         </Col>
-
 
                                         <Col>
                                             <h1 className='countNumber'>50+</h1>
                                             <h4 className='countTitle'>Course Completed</h4>
-                                            <hr className="bg-white w-25"/>
+                                            <hr className='bg-white w-25'/>
                                         </Col>
-
-
                                         <Col>
                                             <h1 className='countNumber'>8+</h1>
                                             <h4 className='countTitle'>Hours Daily Spending on Development</h4>
-                                            <hr className="bg-white w-25"/>
+                                            <hr className="bg-white w-25" />
                                         </Col>
                                     </Row>
-
                                 </Col>
-
-                                <Col className='text-center' lg={8} md={6} sm={12}>
-                                
+                                <Col lg={4} md={6} sm={12}>
+                                    <Card className="workCard" >
+                                        <Card.Body>
+                                            <Card.Title className="cardTitle">What I Have Achieved</Card.Title>
+                                            <Card.Text>
+                                            <p className="cardSubTitle text-justify"> Requirment Gathering </p>
+                                            <p className="cardSubTitle text-justify"> System Analysis </p>
+                                            <p className="cardSubTitle text-justify"> Coding Testing </p>
+                                            <p className="cardSubTitle text-justify"> Implementation </p>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
                                 </Col>
                             </Row>
-                        </Container>
-                        
+                        </Container> 
                     </div>
                 </Container>
             </Fragment>
         )
-   }
+    }
 }
 
 export default Summary
