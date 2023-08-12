4e8a2b7af81b679d405bdd88d428ba17b9452667
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-12 15:26:21 +0600

    Working on Project Component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react'
+import { Container, Row } from 'react-bootstrap'
+
+export class Projects extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default Projects
