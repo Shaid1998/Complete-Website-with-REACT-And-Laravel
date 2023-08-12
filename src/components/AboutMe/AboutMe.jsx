3e4366d0ee525be8051a470944243d0e63782170
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-12 20:03:54 +0600

    Working on About Me Component

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,34 +1,39 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import face from '../../asset/image/face.png'
+import { init } from 'ityped'
 
 class AboutMe extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Container className='text-center'>
-            <h1 className='ServiceMainTitle'>ABOUT ME</h1>
-            <div className='bottom'></div>
-            <Row>
-                <Col lg={6} md={6} sm={12}>
-                    <div className='AboutImage'>
-                        <img className='aboutImg' src={face} />
-                    </div>
-                </Col>
+    componentDidMount(){
+        const myElement = document.querySelector('#myElement')
+        init(myElement, { showCursor: false, strings: ['Web Developer!', 'Web Designer!']})
+    }
+    render() {
+        return (
+        <Fragment>
+            <Container className='text-center'>
+                <h1 className='ServiceMainTitle'>ABOUT ME</h1>
+                <div className='bottom'></div>
+                <Row>
+                    <Col lg={6} md={6} sm={12}>
+                        <div className='AboutImage'>
+                            <img className='aboutImg' src={face} />
+                        </div>
+                    </Col>
 
-                <Col lg={6} md={6} sm={12}>
-                    <div className='aboutMeBody'>
-                        <h2 className='aboutMeDetails'>Hi There, I'm</h2>
-                        <h2 className='aboutMeTitle'>Md. Shaidur Rahman</h2>
-                        <h3 className='aboutMeDetails'>Work as <span>Developer</span></h3>
-                    </div>
-                
-                </Col>
-            </Row>
-        </Container>
-      </Fragment>
-    )
-  }
+                    <Col lg={6} md={6} sm={12}>
+                        <div className='aboutMeBody'>
+                            <h2 className='aboutMeDetails'>Hi There, I'm</h2>
+                            <h2 className='aboutMeTitle'>Md. Shaidur Rahman</h2>
+                            <h3 className='aboutMeDetails'>Work as <span id='myElement'></span></h3>
+                        </div>
+                    
+                    </Col>
+                </Row>
+            </Container>
+        </Fragment>
+        )
+    }
 }
 
 export default AboutMe
