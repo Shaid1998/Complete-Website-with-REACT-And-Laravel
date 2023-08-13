66950e6845a4abd59624539464a4df3fa11c3763
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-13 18:01:58 +0600

    Working On About Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/HomePage';
 import PageTop from './components/PageTop/PageTop';
 import TopNavigation from './components/TopNavigation/TopNavigation';
+import AboutDescription from './components/AboutDescription/AboutDescription';
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <div>
       <TopNavigation />
       <PageTop pagetitle='About Me'/>
+      <AboutDescription />
     </div>
   );
 }
---
+++ src/components/AboutDescription/AboutDescription.jsx
@@ -0,0 +1,58 @@
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+class AboutDescription extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container>
+            <Row>
+                <Col lg={12} md={12} sm={12}>
+                    <h1 className=" serviceName">Who Am I?</h1>
+                    <hr />
+                    <p className="text-justify serviceDescription"> As a recent graduate with a Bachelor of Science in Software
+                        Engineering from Daffodil International University, I possess a strong
+                        foundation in programming languages, including PHP, JavaScript, and
+                        SQL. Although I may be an entry-level candidate, I am confident that
+                        my skills and enthusiasm make me an excellent candidate for this
+                        position.<br></br><br></br>
+
+                        During my time at Daffodil International University University, I
+                        completed multiple projects that enhanced my understanding of web
+                        development and PHP. In one of my major projects.<br></br><br></br>
+
+                        Collaboration and adaptability are two essential qualities that I have
+                        refined through group projects  and I am eager to apply these skills
+                        in a professional setting. Furthermore, I am continuously seeking to
+                        expand my skill set and stay current with industry trends by
+                        participating in coding challenges .<br></br><br></br><br></br>
+                    </p>
+
+                    <h1 className=" serviceName">My Word</h1>
+                    <hr />
+                    <p className="text-justify serviceDescription"> As a recent graduate with a Bachelor of Science in Software
+                        Engineering from Daffodil International University, I possess a strong
+                        foundation in programming languages, including PHP, JavaScript, and
+                        SQL. Although I may be an entry-level candidate, I am confident that
+                        my skills and enthusiasm make me an excellent candidate for this
+                        position.<br></br><br></br>
+
+                        During my time at Daffodil International University University, I
+                        completed multiple projects that enhanced my understanding of web
+                        development and PHP. In one of my major projects.<br></br><br></br>
+
+                        Collaboration and adaptability are two essential qualities that I have
+                        refined through group projects  and I am eager to apply these skills
+                        in a professional setting. Furthermore, I am continuously seeking to
+                        expand my skill set and stay current with industry trends by
+                        participating in coding challenges .<br></br><br></br>
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default AboutDescription
