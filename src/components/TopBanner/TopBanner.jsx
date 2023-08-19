5639026ee635872040763963a93e9baddcdb2a96
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-19 20:53:22 +0600

    api consuming

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -3,11 +3,21 @@ import { Container, Row, Col, Button } from 'react-bootstrap'
 import '../../asset/css/custom.css'
 import '../../asset/css/bootstrap.min.css'
 import axios from 'axios'
+import RestClient from '../../RestAPI/RestClient.js'
+import AppURL from '../../RestAPI/AppURL.js'
 
 class TopBanner extends Component {
-  
+  constructor(){
+    super();
+    this.state={
+      title:'',
+      subtitle:''
+    }
+  }
   componentDidMount(){
-    
+    RestClient.GetRequest(AppURL.HomeTitle).then(result=>{
+      this.setState({title:result[0]['home_title'],subtitle:result[0]['home_sub_title']});
+    })
   }
   render() {
     return (
@@ -17,15 +27,13 @@ class TopBanner extends Component {
                 <Container className='TopContent'>
                     <Row>
                         <Col className='text-center'>
-                            <h1 className='topTitle'>Md. Shaidur Rahman</h1>
-                            <h4 className='topShbTitle'>Web Developer</h4>
+                            <h1 className='topTitle'>{this.state.title}</h1>
+                            <h4 className='topShbTitle'>{this.state.subtitle}</h4>
                             <Button variant='primary'>Learn More</Button>
                         </Col>
                     </Row>
                 </Container>
-
             </div>
-
         </Container>
       </Fragment>
     )
