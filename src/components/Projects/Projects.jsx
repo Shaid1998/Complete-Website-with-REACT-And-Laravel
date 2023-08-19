8e390966ab985fe84511c1cb2c1ebb39f3a519b2
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-19 21:56:02 +0600

    api consuming

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,52 +1,43 @@
 import React, { Component, Fragment } from 'react'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import RestClient from '../../RestAPI/RestClient.js'
+import AppURL from '../../RestAPI/AppURL.js'
 
 export class Projects extends Component {
+    constructor(){
+        super();
+        this.state={
+          projectData:[]
+        }
+      }
+      componentDidMount(){
+        RestClient.GetRequest(AppURL.ProjectDetails).then(result=>{
+          this.setState({projectData:result});
+        })
+      }
+      
+        
   render() {
+    const MyList = this.state.projectData;
+        const MyView = MyList.map(MyList=>{
+          return <Col lg={4} md={6} sm={12}>
+                    <Card className='projectCard'>
+                        <Card.Img variant="top" src={MyList.small_image} />
+                        <Card.Body>
+                            <Card.Title className='serviceName'>{MyList.short_title}</Card.Title>
+                            <Card.Text className='serviceDescription'>{MyList.short_description}</Card.Text>
+                            <Button variant="primary">More</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+        })
     return (
       <Fragment> 
         <Container className='text-center'>
             <h1 className='ServiceMainTitle'>Projects</h1>
             <div className='bottom'></div>
             <Row>
-                <Col lg={4} md={6} sm={12}>
-                    <Card className='projectCard'>
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/web-template-website-design-concept_53876-125608.jpg?w=826&t=st=1691833008~exp=1691833608~hmac=1ac1cd2e300872dbe350de76ed389106d60d94a26f637b0bfefd311bfa57127c" />
-                        <Card.Body>
-                            <Card.Title className='serviceName'>Complete Website with REACT Js and Laravel</Card.Title>
-                            <Card.Text className='serviceDescription'>
-                                This system is built with the help of Laravel framework. React Js are used to design the front. Laravel is used as the backend. MySQL is used as the database. I’m currently working under this project, and working day by day processes based on agile SDLC model.
-                            </Card.Text>
-                            <Button variant="primary">More</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col lg={4} md={6} sm={12}>
-                    <Card className='projectCard'>
-                        <Card.Img variant="top" src="https://img.freepik.com/free-photo/code-coding-programming-technology-technical-concept_53876-120436.jpg?w=996&t=st=1691833155~exp=1691833755~hmac=2c308cd8e5d5dd7160d72f2d681beb19df4bce64d244336a50e0bbac2e57c853" />
-                        <Card.Body>
-                            <Card.Title className='serviceName'>Hostel Management Full System With Laravel</Card.Title>
-                            <Card.Text className='serviceDescription'>
-                                It is a multivendor e-commerce site, there will be many vendors and many users. Separate login system for each. Admin, Vendor, User are the stockholders of this system. This system is built with the help of Laravel framework. HTML, CSS, PHP are used to design the front.
-                            </Card.Text>
-                            <Button variant="primary">More</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col lg={4} md={6} sm={12}>
-                    <Card className='projectCard'>
-                        <Card.Img variant="top" src="https://img.freepik.com/premium-photo/php-programming-code-abstract-technology-background_272306-152.jpg?w=1060" />
-                        <Card.Body>
-                            <Card.Title className='serviceName'>laravel multivendor ecommerce full project</Card.Title>
-                            <Card.Text className='serviceDescription'>
-                                It is a multivendor e-commerce site, there will be many vendors and many users. Separate login system for each. Admin, Vendor, User are the stockholders of this system. This system is built with the help of Laravel framework. HTML, CSS, PHP are used to design the front      . 
-                            </Card.Text>
-                            <Button variant="primary">More</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                {MyView}
             </Row>
         </Container>
       </Fragment>
